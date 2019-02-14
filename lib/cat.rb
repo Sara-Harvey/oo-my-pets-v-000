be5ee1e12ed6dddcbf6f83b9325a26cba8825839
Sara Harvey <saraharveynyc@gmail.com>
2019-02-14 11:16:39 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+attr_accessor :pets, :mood, :owner
+
+def initialize(name, mood = "nervous")
+@name = name
+@mood = mood
+end
+
+def name
+  @name
+end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,73 @@
 class Owner
-  # code goes here
+attr_accessor :name, :fish, :dog, :cat, :mood
+attr_reader :species, :pets
+
+@@all = []
+@@count = 0
+
+def initialize(name)
+@pets = {fishes: [], cats: [], dogs: []}
+@@all << self
+@@count +=1
+@species = "human"
+end
+
+def self.all
+  @@all
+end
+
+def Owner.count
+  @@count
+end
+
+def self.reset_all
+  @@count = 0
+end
+
+def say_species
+  "I am a human."
+end
+
+def buy_fish(name)
+  @pets[:fishes] << Fish.new(name)
+  @pets
+end
+
+def buy_cat(name)
+  @pets[:cats] << Cat.new(name)
+  @pets
+end
+
+def buy_dog(name)
+  @pets[:dogs] << Dog.new(name)
+  @pets
+end
+
+def walk_dogs
+  buy_dog(name)
+  @pets[:dogs].each do |dog|
+    dog.mood = "happy"
+  end
+end
+
+def play_with_cats
+  buy_cat(name)
+  @pets[:cats].each do |cat|
+    cat.mood = "happy"
+end 
+end
+
+def feed_fish
+  buy_fish(name)
+  @pets[:fishes].each do |fish|
+    fish.mood = "happy"
+end
+end
+
+def Owner.sell_pets
+Owner.pets.each do |key, value|
+  mood = "nervous"
+end
+end
+  
 end
